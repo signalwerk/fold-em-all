401390417f66fa2820f3a97c9285a61c144b9b26
alex jenter <alex@attribute.ch>
2018-07-24 14:24:34 +0200

    ADD slate js

--- src/components/Alex/index.js
+++ src/components/Alex/index.js
@@ -1,10 +1,43 @@
 import React, { Component, Fragment } from 'react'
+import { Editor } from 'slate-react'
+import { Value } from 'slate'
+
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'A line of text in a paragraph.',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+})
 
 export default class Alex extends Component {
+  // Set the initial value when the app is first constructed.
+  state = {
+    value: initialValue,
+  }
+
+  // On change, update the app's React state with the new editor value.
+  onChange = ({ value }) => {
+    this.setState({ value })
+  }
+
   render() {
       return (
         <Fragment>
-          <p>hellö mundo</p>
+          <Editor value={this.state.value} onChange={this.onChange} />
         </Fragment>
       )
   }

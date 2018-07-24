070f083ab1e13a783c9964955cacee87b7e1e3d3
siganalwerk <sh@signalwerk.ch>
2018-07-24 14:15:08 +0200

    ADD: init

---
+++ src/components/Alex/index.js
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react'
+
+export default class Alex extends Component {
+  render() {
+      return (
+        <Fragment>
+          <p>hellö mundo</p>
+        </Fragment>
+      )
+  }
+}

77110e404d372efb41b85822737bbce472cd858b
Jonas Shriqui <jonas.shriqui@gmail.com>
2018-07-26 14:07:10 +0200

    Changed home layout

--- src/components/PostTeaser/index.js
+++ src/components/PostTeaser/index.js
@@ -8,12 +8,9 @@ export default class PostTeaser extends Component {
       <Fragment>
         <article className="postTeaser--root">
           <Link className="postTeaser--link" to={`/post/${this.props.post.id}`}>
-            <div className="postTeaser--inner">
-              <div className="postTeaser--hover">→</div>
-              <h1>!!!{this.props.post.title}</h1>
-              <h3>By {this.props.post.author.name}</h3>
-            </div>
+            <h1><span>!!!{this.props.post.title}</span></h1>
           </Link>
+          <h3>By {this.props.post.author.name}</h3> 
         </article>
       </Fragment>
     )

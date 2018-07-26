29721bf5ba5acba7e94aac93b636a1d68ec628b0
alex jenter <alex@attribute.ch>
2018-07-26 16:05:00 +0200

    separate switches and hotkeys

--- src/components/Editor/slate-hotkeys.js
+++ src/components/Editor/slate-hotkeys.js
@@ -1,8 +1,3 @@
-import React from 'react'
-import Code from './components/Code'
-import Image from './components/Image'
-import Title from './components/Title'
-
 const MarkHotkey = options => {
   const { type, key } = options
   return {
@@ -27,28 +22,6 @@ const BlockHotkey = options => {
       return true
     }
   }
-}
-
-export const NodeSwitch = props => {
-  const { attributes, children, isFocused, node } = props
-  const src = node.type === 'image'
-    ? node.data.get('src')
-    : undefined
-
-  return {
-    code: <Code {...props} />,
-    heading: <Title>{children}</Title>,
-    image: <Image src={src} selected={isFocused} {...attributes} />
-  }[node.type] || <p>{children}</p>
-}
-
-export const MarkSwitch = props => {
-  const { children, mark } = props
-
-  return {
-    italic: <em>{children}</em>,
-    negative: <span className="mark-negative">{children}</span>,
-  }[mark.type] || children
 }
 
 export const hotkeys = [
